bf86f58e1298bae38aa06de434c334c4f4833880
kumar-dange <121814760+kumar-dange@users.noreply.github.com>
2024-12-10 20:34:03 +0000

    clean up

--- ca1_kumar_dange_10637925.py
+++ ca1_kumar_dange_10637925.py
@@ -13,7 +13,6 @@ Original file is located at
 import requests
 import json
 import pandas as pd
-pip install pandas
 
 url = 'https://www.arnotts.ie/content/arnotts/ie/en/plp/jcr:content/root/container/plpcontainer.json?pagePath=https://www.arnotts.ie/men/shoes/&q%3Dmenu%3Dtrue%26offset%3D36%26limit%3D36&siteID=arnotts'
 
